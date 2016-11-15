51622fdfb6312a1dfac1ef92081bc1eb91576ac5
Rafaela <rafaela.vm@gmail.com>
2016-11-15 16:46:33 -0200

    Aula 04: Minimizando a complexidade do nosso HTML - sem exercicio

---
+++ public/js/directives/minhas-diretivas.js
@@ -0,0 +1,43 @@
+angular.module('minhasDiretivas',[])
+.directive('meuPainel',function(){
+    
+    var ddo = {};
+    
+    //diretiva de atributo e de elemento
+    ddo.restrict = "AE";
+    
+    //escopo privado da diretiva, pra ela ficar isolada
+    //não é o mesmo scope do controller, ele é só da diretiva
+    //titulo: é passado como parâmetro
+    //<meu-painel titulo="titulo"></meu-painel>
+    ddo.scope = {
+        
+        //'@titulo' //quando é o mesmo nome (propriedade e atributo pode só colocar o @)
+        titulo: '@' 
+    };
+    
+    //diretiva que tem elementos filhos e você quer manter esses elementos
+    //pra isso se usa o transclude
+    //no caso é a imagem dentro do panel:
+    //<meu-painel ng-repeat="foto in fotos" titulo="{{foto.titulo}}">
+    //  <img src="{{foto.url}}"  alt="{{foto.titulo}}" class="img-responsive center-block"/>
+    //  </meu-painel>
+    //no ddo.template coloca centro da class 'panel-body' a tag ng-transclude
+
+    ddo.transclude = true;
+    
+    //template da diretiva
+    /*ddo.template = '<div class="panel panel-default">'
+               +         '<div class="panel panel-heading">'
+               +             '<h3 class="panel-title text-center">{{titulo}}</h3>'
+               +         '</div>'
+               +         '<div class="panel-body" ng-transclude>'
+               +         '</div>'
+               +   '</div>';*/
+    ddo.templateUrl = 'js/directives/meu-painel.html';
+    
+    return ddo;
+    
+    
+});
+
