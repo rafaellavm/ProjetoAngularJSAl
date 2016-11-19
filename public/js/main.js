e942cfb5de9174bd60c47ea8fc7747a56f914c24
Rafaela <rafaela.vm@gmail.com>
2016-11-19 18:30:22 -0200

    aula 06: "dividir pra conquistar" :  inserção de rotas

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
-angular.module('alurapic',['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic',['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider){
+
+    //assim o angular para de trabalhar com hash e passa a trabalhar com html5, tirando o "#" do link
+    //no html é chamado dentro do head <base href="/">
+    $locationProvider.html5Mode(true);
+
+    //configuração das rotas
+    //vai ser inserida através da <ng-view> dentro do index.html
+    //nessa rota aparece a lista de cadastros:
+    $routeProvider.when('/fotos',{
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
+    });
+
+    //nessa rota chama a tela de adição de contato
+    $routeProvider.when('/fotos/new',{
+        templateUrl: 'partials/foto.html' //,
+        //controller: 'FotosController'
+    });
+
+    //otherwise: 'caso contrário'... É chamada caso seja digitado uma página que não existe
+    $routeProvider.otherwise({
+        redirectTo: '/fotos'
+    });
+
+});
