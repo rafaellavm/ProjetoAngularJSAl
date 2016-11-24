9f4f399511611523362aae4301f11824490208c5
rafaelavm <rafaela.vm@gmail.com>
2016-11-24 17:17:11 -0200

    Aula 07. Precisamos alimentar esse sistema - criação do formulário de cadastro de foto e sua validação

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,11 +1,23 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function ($scope, $http) {
 
-    $scope.foto = {};
+	$scope.foto = {};
+	$scope.mensagem = '';
 
-    $scope.submeter = function(){
+	//método post: para adicionar um registro no back end
+	$scope.submeter = function() {
 
-        console.log($scope.foto);
-    };
+			if ($scope.formulario.$valid) {
 
+				$http.post('/v1/fotos', $scope.foto)
+				.success(function() {
+					$scope.foto = {};
+					$scope.mensagem = 'Foto cadastrada com sucesso';
+				})
+				.error(function(erro) {
+					console.log(erro);
+					$scope.mensagem = 'Não foi possível cadastrar a foto';
+				})
 
+			}
+		};
 });
--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,77 +1,55 @@
-angular.module('minhasDiretivas',[])
-.directive('meuPainel',function(){
+angular.module('minhasDiretivas', [])
+	.directive('meuPainel', function() {
+
+		var ddo = {};
     
-    var ddo = {};
+        //diretiva de atributo e de elemento
+        ddo.restrict = "AE";
     
-    //diretiva de atributo e de elemento
-    ddo.restrict = "AE";
-    
-    //escopo privado da diretiva, pra ela ficar isolada
-    //não é o mesmo scope do controller, ele é só da diretiva
-    //titulo: é passado como parâmetro
-    //<meu-painel titulo="titulo"></meu-painel>
-    ddo.scope = {
-        
-        //'@titulo' //quando é o mesmo nome (propriedade e atributo pode só colocar o @)
-        titulo: '@' 
-    };
-    
-    //diretiva que tem elementos filhos e você quer manter esses elementos
+     //diretiva que tem elementos filhos e você quer manter esses elementos
     //pra isso se usa o transclude
     //no caso é a imagem dentro do panel:
     //<meu-painel ng-repeat="foto in fotos" titulo="{{foto.titulo}}">
     //  <img src="{{foto.url}}"  alt="{{foto.titulo}}" class="img-responsive center-block"/>
     //  </meu-painel>
     //no ddo.template coloca centro da class 'panel-body' a tag ng-transclude
+        ddo.transclude = true;
 
-    ddo.transclude = true;
+        //escopo privado da diretiva, pra ela ficar isolada
+        //não é o mesmo scope do controller, ele é só da diretiva
+        //titulo: é passado como parâmetro
+        //<meu-painel titulo="titulo"></meu-painel>
+
+        ddo.scope = {
+            //'@titulo' //quando é o mesmo nome (propriedade e atributo pode só colocar o @)
+            titulo: '@'
+        };
     
-    //template da diretiva
-    /*ddo.template = '<div class="panel panel-default">'
+        //template da diretiva
+        /*ddo.template = '<div class="panel panel-default">'
                +         '<div class="panel panel-heading">'
                +             '<h3 class="panel-title text-center">{{titulo}}</h3>'
                +         '</div>'
                +         '<div class="panel-body" ng-transclude>'
                +         '</div>'
                +   '</div>';*/
-    ddo.templateUrl = 'js/directives/meu-painel.html';
-    
-    return ddo;
-    
-}).directive('minhaFoto', function(){
 
-    var ddo = {};
+        ddo.templateUrl = 'js/directives/meu-painel.html';
 
-    ddo.restrict = 'AE';
+		return ddo;
+	})
+    .directive('minhaFoto', function() {
 
-    ddo.scope = {
+        var ddo = {};
 
-        titulo: '@',
-        url: '@'
-    };
+        ddo.restrict = "AE";
 
-    ddo.template = '<img class="img-responsive center-block" src="{{url}}" titulo="{{titulo}}" />';
-    
-});
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
 
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return ddo;
+    });
